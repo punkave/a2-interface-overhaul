945e622ebfca38b8f94cda7c44879dc58b2e963e
Kyle Stetz <kyle@punkave.com>
2015-09-04 10:57:20 -0400

    added some tabs; added tab highlighting

--- public/js/modules/site.js
+++ public/js/modules/site.js
@@ -2,17 +2,31 @@ $( function() {
 
   var $overlay = $('.overlay');
 
+  // ================================================================
+  // SCREENS
+  // ================================================================
+
+  // open a screen
+
   $('[data-open]').on('click', function(e) {
     $('[data-screen="' + $(this).attr('data-open') + '"]')
       .toggleClass('screen--open', true);
     $overlay.toggleClass('overlay--open', true);
   });
 
+  // close a screen
+
   $('[data-close]').on('click', function(e) {
     $('[data-screen="' + $(this).attr('data-close') + '"]')
       .toggleClass('screen--open', false);
     $overlay.toggleClass('overlay--open', false);
   });
+
+  // ================================================================
+  // SECTIONS
+  // ================================================================
+
+  // go to a section
 
   $('[data-open-section]').on('click', function(e) {
     $('[data-section]').addClass('hidden');
@@ -27,6 +41,16 @@ $( function() {
     $('.context-section[data-section="' + target + '"]')
         .toggleClass('hidden', false);
 
+  });
+
+  // ================================================================
+  // TABS
+  // ================================================================
+
+  // highlight a tab
+
+  $('.tab').on('click', function(e) {
+    $(this).addClass('tab--active').siblings().removeClass('tab--active');
   });
 
 });
